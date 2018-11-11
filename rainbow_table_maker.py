98d024e59126f968474f4e4eb0186e5950db5add
xoonel <44796822+xoonel@users.noreply.github.com>
2018-11-11 11:08:10 +0100

    Update rainbow_table_maker.py
    Added comments, made some style preferences changes, and added ability to export results to .txt file (see COMMENTS on lines: 7 & 15).

--- rainbow_table_maker.py
+++ rainbow_table_maker.py
@@ -1,11 +1,17 @@
 import itertools
 
-chrs = 'ABCDEFG'
+chrs = 'ABCDEF'
+
 cnt = 0
+
+# Creates the rainbow table text document; overwrites previous ones if program is run.
+rnbwtbl_file = open("rainbow_table.txt", "w")
 
 while cnt <= len(chrs):
 
-    for xs in itertools.product(chrs, repeat=cnt):
-        print(''.join(xs)) 
-    
+    for i in itertools.product(chrs, repeat=cnt):
+        # Prints all possible character permutations in the shell.
+        print(''.join(i)) 
+        # Concurrently writes all possible possible character permutations to text document () contiguously with no formatting.
+        rnbwtbl_file.write(''.join(i))
     cnt = cnt+1
